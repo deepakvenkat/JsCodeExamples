e56764fac4ac301d90db2916799e17a2d49dd2ff
deepakvenkat <dipak2911@gmail.com>
2020-05-17 15:10:51 -0400

    Add iterable generator example.

--- examples/generator.js
+++ examples/generator.js
@@ -10,6 +10,10 @@ function getTotalTicketAvailable () {
 	return 3;
 }
 
+function createUsers (userCount, prefix) {
+	return [...Array(userCount).keys()]
+		.map((key) => `${prefix} user ${key}`);
+}
 /** 
 * A generator function is one where you can yield the execution call. 
 * It returns a special `Generator object`
@@ -22,6 +26,9 @@ function * generateTickets  () {
 	while (currentCount < totalTicketCount) {
 		yield ++currentCount;
 	}
+	// you can't do the following 
+	// becuase yield is no longer in the context of a generator.
+	// [...Array(totalTicketCount).keys()].forEach((key) => yield key);
 	return;
 }
 
@@ -38,11 +45,40 @@ function getTickets (users) {
 	});
 }
 
-let q = ['user1', 'user2', 'user3', 'user4', 'user5'];
+let q = createUsers(5, 'test1');
 getTickets(q);
 
 
 // the generator object should be regenerated
-q = ['user 6', 'user 7'];
+q = createUsers(2, 'test2');
 getTickets(q);
+
+
+/**
+* generators also return a iterable object
+**/
+function iterateOverTickets1 (users) {
+	let i = 0;
+	for (const ticket of generateTickets()) {
+		console.log(`Congrats ${users[i]} you have won a ticket, no: ${ticket}`);
+		i++;
+	}
+	console.log(`Sorry users ${users.slice(i)}, no more tickets available`);
+}
+
+function iterateOverTickets2 (users) {
+	let i = 0;
+	const tickets = [...generateTickets];
+	tickets.forEach((ticket) => {
+		console.log(`Congrats ${users[i]} you have won a ticket, no: ${ticket}`);
+		++i;
+	});
+	console.log(`Sorry users ${users.slice(i)}, no more tickets available`);
+}
+
+
+q = createUsers(5, 'test3');
+// should be the same output
+iterateOverTickets1(q);
+iterateOverTickets1(q);
 
